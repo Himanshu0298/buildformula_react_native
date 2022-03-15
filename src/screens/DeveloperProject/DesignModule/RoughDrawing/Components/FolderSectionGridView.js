0c5603d21cd9d257dd54f7cadb6d8be5d78d48dd
Developer <developer@rrinfotech.com>
2022-03-15 19:46:36 +0530

    design module -rough module UI

---
+++ src/screens/DeveloperProject/DesignModule/RoughDrawing/Components/FolderSectionGridView.js
@@ -0,0 +1,112 @@
+import React from 'react';
+import {StyleSheet, View, Image, TouchableOpacity} from 'react-native';
+import {Text} from 'react-native-paper';
+import FolderIcon from 'assets/images/folder_icon.png';
+import {useSelector} from 'react-redux';
+import {FlatList} from 'react-native-gesture-handler';
+import OpacityButton from 'components/Atoms/Buttons/OpacityButton';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+function RenderFolder(props) {
+  const {
+    folder,
+    toggleMenu,
+    navigation,
+    setModalContentType,
+    folderIndex,
+    setModalContent,
+  } = props;
+  const {folder_name} = folder;
+
+  return (
+    <View style={styles.folderContainer}>
+      <View style={styles.folderIconContainer}>
+        <Image source={FolderIcon} style={styles.PdfIcon} />
+        <OpacityButton
+          opacity={0.01}
+          onPress={() => {
+            toggleMenu(folderIndex);
+            setModalContentType('menu');
+            setModalContent(folder);
+          }}>
+          <MaterialCommunityIcons
+            name="dots-vertical"
+            size={20}
+            color="black"
+          />
+        </OpacityButton>
+      </View>
+      <View>
+        <Text numberOfLines={2} style={styles.text}>
+          {folder_name}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const FolderSectionGridView = props => {
+  const {route, menuId, toggleMenu, setModalContentType, setModalContent} =
+    props;
+
+  const {index_of: folderDepth = 0} = route?.params || {};
+
+  const {folders} = useSelector(s => s.files);
+
+  const filteredFolders = folders?.[folderDepth] || [];
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={filteredFolders}
+        extraData={filteredFolders}
+        numColumns={3}
+        showsVerticalScrollIndicator={false}
+        keyboardShouldPersistTaps="handled"
+        keyExtractor={index => index.toString()}
+        renderItem={({item, index}) => (
+          <RenderFolder
+            {...props}
+            folder={item}
+            key={index}
+            menuId={menuId}
+            toggleMenu={toggleMenu}
+            setModalContentType={setModalContentType}
+            folderDepth={folderDepth}
+            folderIndex={filteredFolders.indexOf(item)}
+            setModalContent={setModalContent}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#ffffff',
+  },
+  folderContainer: {
+    flex: 1,
+  },
+  PdfIcon: {
+    width: 48,
+    height: 48,
+    paddingLeft: 10,
+    marginBottom: 10,
+  },
+
+  folderIconContainer: {
+    flexDirection: 'row',
+    margin: 5,
+    alignItems: 'flex-start',
+    paddingHorizontal: 10,
+  },
+  text: {
+    paddingBottom: 10,
+    paddingHorizontal: 15,
+  },
+});
+
+export default FolderSectionGridView;
--- src/screens/DeveloperProject/DesignModule/RoughDrawing/RoughDrawing.js
+++ src/screens/DeveloperProject/DesignModule/RoughDrawing/RoughDrawing.js
@@ -30,6 +30,9 @@ import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityI
 import Feather from 'react-native-vector-icons/Feather';
 import dayjs from 'dayjs';
 import {getFileExtension} from 'utils/download';
+import NoResult from 'components/Atoms/NoResult';
+import Fontisto from 'react-native-vector-icons/Fontisto';
+import Foundation from 'react-native-vector-icons/Foundation';
 import DeleteDialog from './Components/DeleteDialog';
 import RenameDialogue from './Components/RenameDialog';
 import CreateFolderDialogue from './Components/CreateFolderDialog';
@@ -37,6 +40,7 @@ import MenuDialog from './Components/MenuDialog';
 import VersionDialog from './Components/VersionDialog';
 import FoldersSection from './Components/FoldersSection';
 import ShareDialogue from './Components/ShareDialogue';
+import FolderSectionGridView from './Components/FolderSectionGridView';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const relativeTime = require('dayjs/plugin/relativeTime');
@@ -212,6 +216,29 @@ function RenderMenuModal(props) {
   );
 }
 
+const ListViewControl = props => {
+  const {theme, listMode, setListMode} = props;
+  return (
+    <View style={styles.headerActionContainer}>
+      <TouchableOpacity onPress={() => setListMode('list')}>
+        <Foundation
+          name="list-bullet"
+          size={20}
+          color={listMode === 'list' ? theme.colors.primary : undefined}
+          style={styles.headerIcon}
+        />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => setListMode('grid')}>
+        <Fontisto
+          name="nav-icon-grid"
+          size={17}
+          color={listMode === 'grid' ? theme.colors.primary : undefined}
+        />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
 function RoughDrawing(props) {
   const {theme, route, navigation} = props;
   const {folder_name: folderName, index_of: folderDepth = 0} =
@@ -250,6 +277,8 @@ function RoughDrawing(props) {
   const [modalContent, setModalContent] = React.useState({});
   const [shareDialog, setShareDialog] = React.useState(false);
   const [DialogType, setDialogType] = React.useState();
+  const [listMode, setListMode] = React.useState('list');
+
   // const [selectedUploadFile, setSelectedUploadFile] = React.useState();
 
   React.useEffect(() => {
@@ -268,7 +297,6 @@ function RoughDrawing(props) {
       color: theme.colors.primary,
       label: 'Create new folder',
       onPress: () => {
-        toggleFab();
         toggleDialog('createFolder');
       },
     },
@@ -419,10 +447,22 @@ function RoughDrawing(props) {
             />
           </View>
         ) : null}
-        <FoldersSection
-          {...props}
-          {...{menuId, toggleMenu, setModalContent, setModalContentType}}
-        />
+        <View style={styles.headerContainer}>
+          <Subheading>Rough Drawing</Subheading>
+          <ListViewControl {...props} {...{listMode, setListMode}} />
+        </View>
+        {listMode === 'list' ? (
+          <FoldersSection
+            {...props}
+            {...{menuId, toggleMenu, setModalContent, setModalContentType}}
+          />
+        ) : null}
+        {listMode === 'grid' ? (
+          <FolderSectionGridView
+            {...props}
+            {...{menuId, toggleMenu, setModalContent, setModalContentType}}
+          />
+        ) : null}
       </ScrollView>
       {modulePermissions?.editor || modulePermissions?.admin ? (
         <FAB.Group
@@ -433,7 +473,8 @@ function RoughDrawing(props) {
           }}
           icon="plus"
           small
-          onPress={toggleFab}
+          // onPress={toggleFab}
+          onPress={() => toggleDialog('createFolder')}
           onStateChange={() => {
             console.log('-----> onStateChange');
           }}
@@ -478,13 +519,6 @@ function RoughDrawing(props) {
         dialogueContent={modalContent}
         renameFolderHandler={renameFolderHandler}
       />
-      {/* <UploadDialog
-        {...props}
-        visible={DialogType === 'uploadFile'}
-        toggleDialogue={toggleDialog}
-        selectedUploadFile={selectedUploadFile}
-        handleFileUpload={handleFileUpload}
-      /> */}
       <DeleteDialog
         visible={DialogType === 'deleteFileFolder'}
         toggleDialogue={toggleDialog}
@@ -573,6 +607,21 @@ const styles = StyleSheet.create({
   emptyContainer: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
+    paddingLeft: 10,
+    paddingRight: 30,
+  },
+  headerActionContainer: {
+    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  headerIcon: {
+    marginHorizontal: 10,
   },
 });
 
