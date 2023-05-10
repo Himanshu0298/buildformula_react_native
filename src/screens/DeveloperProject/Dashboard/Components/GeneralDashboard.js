d480c64f4bef895cabc67b1f54da50c5f37edcb8
Himanshu0298 <106006151+Himanshu0298@users.noreply.github.com>
2023-05-10 15:19:22 +0530

    wip

--- src/screens/DeveloperProject/Dashboard/Components/GeneralDashboard.js
+++ src/screens/DeveloperProject/Dashboard/Components/GeneralDashboard.js
@@ -1,46 +1,49 @@
 import React from 'react';
-import {
-  Image,
-  RefreshControl,
-  ScrollView,
-  StyleSheet,
-  View,
-} from 'react-native';
-import Layout from 'utils/Layout';
-import {Text, withTheme} from 'react-native-paper';
+import {Image, StyleSheet, View} from 'react-native';
+import {Subheading, Text, withTheme} from 'react-native-paper';
 import {getShadow} from 'utils';
 import {useSelector} from 'react-redux';
-import {BASE_API_URL} from 'utils/constant';
+import {SITE_URL} from 'utils/constant';
+import dayjs from 'dayjs';
+import * as Progress from 'react-native-progress';
+import ImageProgress from 'react-native-image-progress';
+import developer_building from 'assets/images/developer_building.png';
 
-function GeneralDashboard(props) {
-  const {onRefresh} = props;
-
+function GeneralDashboard() {
   const {selectedProject} = useSelector(s => s.project);
-  console.log(
-    '🚀 ~ file: GeneralDashboard.js:12 ~ GeneralDashboard ~ selectedProject:',
-    `http://104.198.176.6/logo_images/${selectedProject.company_logo_url}`,
-  );
+  const {user} = useSelector(s => s.user);
 
-  // http://104.198.176.6/logo_images/1672048141.png
+  const {first_name, last_name, profile_url} = user || {};
 
-  const company_logo = selectedProject.company_logo_url;
+  const company_logo = `${SITE_URL}/logo_images/${selectedProject.company_logo_url}`;
 
   return (
     <View style={styles.staticsContainer}>
-      <ScrollView
+      <View
         showsVerticalScrollIndicator={false}
-        refreshControl={
-          <RefreshControl refreshing={false} onRefresh={onRefresh} />
-        }>
-        <Text>Data</Text>
-        {/* <Image
-          source={{
-            url: `http://104.198.176.6/logo_images/${}`,
-          }}
-          width={50}
-          height={50}
-        /> */}
-      </ScrollView>
+        style={styles.detailsContainer}>
+        {profile_url ? (
+          <ImageProgress
+            source={{uri: company_logo}}
+            indicator={Progress.Pie}
+            indicatorProps={{
+              size: 80,
+              borderWidth: 0,
+              color: 'rgba(234,236,241,1)',
+              unfilledColor: 'rgba(211,218,239,1)',
+            }}
+            style={styles.profileIMG}
+          />
+        ) : (
+          <Image source={developer_building} style={styles.profileIMG} />
+        )}
+        <Text style={styles.subheading}>
+          {first_name} {last_name}
+        </Text>
+        <Subheading style={styles.timeContainer}>
+          {dayjs().format('hh:mm:ss A')}
+        </Subheading>
+      </View>
     </View>
   );
 }
@@ -49,6 +52,27 @@ const styles = StyleSheet.create({
   staticsContainer: {
     backgroundColor: '#eaeff1',
     flexGrow: 1,
+  },
+  profileIMG: {
+    width: 210,
+    height: 210,
+    borderRadius: 100,
+    overflow: 'hidden',
+    ...getShadow(5),
+  },
+  subheading: {
+    marginTop: 30,
+    fontWeight: '600',
+    fontSize: 30,
+  },
+  detailsContainer: {
+    alignItems: 'center',
+    flex: 1,
+    marginTop: 60,
+  },
+  timeContainer: {
+    marginTop: 15,
+    color: '#8e8e8e',
   },
 });
 
